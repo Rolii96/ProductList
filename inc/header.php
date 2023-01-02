36bf50fdbc65f2736f39b9ab01c17e4892cd5eb5
Roland <79096928+Rolii96@users.noreply.github.com>
2023-01-02 12:25:23 +0100

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Product List Page">
+    <meta name="author" content="Roland Ajeti">
+    <link rel="icon" href="images/favicon.png">
+    <title>Product List</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- Start Navabar-->
+    <nav class="navbar navbar-expand-md navbar-dark bg-danger fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><i class="fa fa-code"></i> Roland Ajeti</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+  
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <!-- <a class="nav-link" href="#">Products <span class="sr-only">(current)</span></a> -->
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+    <!-- Start Hero Image -->
+    <div class="hero-image">
+        <div class="hero-text">
+            <h1>Product List Website</h1>
+            <!-- <p></p> -->
+            <a href="https://www.youtube.com/channel/UCA7Tj3B9h2FikEHcEJI4cnQ" target="_blank" class="btn btn-danger btn-lg"><i class="fa fa-youtube"></i>My Youtube Channel</a href="youtube.com">
+        </div>
+    </div>
+    <!-- End Hero Image -->
